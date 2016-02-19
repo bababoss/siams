f9ab2d856960275f442e58d7de1c9336f39a6a0b
baratsush <suresheca2012@gmail.com>
2016-02-19 14:25:25 +0530

    edit raspberry interface

--- InsertSData.py
+++ InsertSData.py
@@ -3,13 +3,26 @@
 #import MyDB connection module
 import MyDB
 db = MyDB.DB()
-i=12
-j=22
-k=33
-db.execute("INSERT INTO Zones (user_id,zone1) \
-      VALUES (%s, NOW() )", [i]);
+import Rpi.GPIO as GPIO
+import time
 
+sensorPin1 = 18 #Broadcom pin 18
+#pin setup:
+GPIO.setmode(GPIO.BCM) #Broadcom pin numbering scheem
+GPIO.setup(sensorPin1, GPIO.IN, pull_up_down=GPIO.PUD_UP) # sensor pin set as input
+user_id = 1023
+data = [user_id]
 
-db.commit()
-print "Records created successfully";
-db.close()
+try:
+    while 1:
+        if GPIO.input(sensorPin1):
+            db.execute("INSERT INTO Zones (user_id,zone1) \
+                  VALUES (%s, NOW() )", [data]);
+
+            db.commit()
+            print "Records created successfully";
+            db.close()
+
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+
+    GPIO.cleanup() # cleanup all GPIO
